a58656d042d13ff57eaaf2871c7035c77c626a0a
VasiliKnyazev <basil_jr_king@mail.ru>
2021-01-10 13:21:40 +0600

    Initial commit

---
+++ src/test/java/com/luxoft/education/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.education.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
